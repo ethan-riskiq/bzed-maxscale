fd3ae2369f8558412056d3b3f1105c092b97ffc7
eshrago <ethan.shrago@riskiq.net>
2016-10-27 11:06:20 -0700

    maxscale cleanup

--- manifests/params.pp
+++ manifests/params.pp
@@ -1,6 +1,4 @@
 class maxscale::params {
     $package_name = 'maxscale'
     $gpg_key_url = 'https://yum.mariadb.org/RPM-GPG-KEY-MariaDB'
-    $gpg_key_id = '70E4618A8167EE24'
-    $repository_base_url = 'http://downloads.mariadb.org/mariadb/repositories/'
 }
